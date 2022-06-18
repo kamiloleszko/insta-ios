f3bac28745688fb081162c85825a9d31b80cfa10
kamiloleszko <fm.kamil.oleszko@gmail.com>
2022-06-18 20:36:57 +0200

    fetch and display users

--- InstaTut/Controller/SearchController.swift
+++ InstaTut/Controller/SearchController.swift
@@ -7,12 +7,55 @@
 
 import UIKit
 
-class SearchController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class SearchController: UITableViewController {
     
+    // MARK: - Properties
+    
+    private var users = [User]()
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureTableView()
+        fetchUsers()
+        
     }
     
+    // MARK: - Api
+    
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    func configureTableView() {
+        view.backgroundColor = .white
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 64
+    }
+    
+}
+
+// MARK: - UiTableViewDataSource
+
+extension SearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.user = users[indexPath.row]
+        cell.backgroundColor = .white
+        return cell
+    }
 }
 
