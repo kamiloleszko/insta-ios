cf257f78a8522513d6ee6eca2d7ba936235ee712
kamiloleszko <fm.kamil.oleszko@gmail.com>
2022-06-24 11:20:46 +0200

    upload post UI

--- InstaTut/Controller/ImageSelectorController.swift
+++ InstaTut/Controller/ImageSelectorController.swift
@@ -11,7 +11,7 @@ class ImageSelectorcontroller: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .white
     }
     
 }
---
+++ InstaTut/Utils/InputTextView.swift
@@ -0,0 +1,12 @@
+//
+//  InputTextView.swift
+//  InstaTut
+//
+//  Created by kamil on 24/06/2022.
+//
+
+import UIKit
+
+class InputTextView: UITextView {
+    
+}
